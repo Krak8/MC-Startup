4ceaa90c6170e2941171fe8e0cc7ad556a412230
Krak8 <87670417+Krak8@users.noreply.github.com>
2022-08-02 15:09:22 +0530

    Fixed issue with no gui flag

--- AdvanceStart.bat
+++ AdvanceStart.bat
@@ -145,9 +145,9 @@ if %Standalone%==true (
     if %AutoRestart%==true (GOTO RESTARTS) ELSE (GOTO STARTS)
 )
 if %GUI%==true set %GUI%=
-if %GUI%==false (
-    if %AutoDownload%==true set GUI=--mc.nogui
-    else set GUI=--nogui
+if %GUI%==false set GUI=--nogui
+if %AutoDownload%==true (
+    if %GUI%==--nogui set GUI=--mc.nogui
 )
 if %Flags%==true (
     if %HFlags%==true set FlagsT=-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true -Dusing.aikars.flags=https://mcflags.emc.gs
